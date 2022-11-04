eb5d602b90ff21ef0a68baad52db2e4df033f2d4
arsyadal <116419335+arsyadal@users.noreply.github.com>
2022-11-04 08:04:50 +0700

    Add files via upload

---
+++ MODUL1/src/Menu.java
@@ -0,0 +1,23 @@
+public class Menu 
+    // TODO Create Attribute of Menu; Name, Category, and Price then Create Setter
+    public String name;
+    public String category;
+    public int Price;
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public void setPrice(int price) {
+        Price = price;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public int getPrice() {
+        return Price;
+    }
---
+++ MODUL1/src/User.java
@@ -0,0 +1,27 @@
+import java.util.Locale.Category;
+
+public class User {
+
+    // TODO Create Attribute of User; Name and Phone Number then Create Setter
+    String name;
+    String category;
+    Integer price;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setCategory(String category){
+        this.category = category;
+    }
+
+    public void setPrice(Integer price){
+        this.price = price;
+    }
+
+
+
+
+    // TODO Create Method to Register User and Display User's Name and Phone Number and success message
+
+
